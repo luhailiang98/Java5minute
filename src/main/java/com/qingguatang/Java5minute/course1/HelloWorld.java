db6b34c8c62e7a842c0f570c3ca12b0eab5e36de
luhailiang <1176239106@qq.com>
2018-07-17 22:13:26 +0800

    hello

---
+++ src/main/java/com/qingguatang/Java5minute/course1/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.qingguatang.Java5minute.course1;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 功能描述: HelloWorld
+ *
+ * @param:
+ * @return: meassage
+ * @auther: luhailiang
+ * @date: 2018/7/17 22:11
+ */
+@Controller
+public class HelloWorld {
+
+  @RequestMapping(value = "/")
+  @ResponseBody
+  public String Hello() {
+    String meassage = "hello";
+    return meassage;
+  }
+}
